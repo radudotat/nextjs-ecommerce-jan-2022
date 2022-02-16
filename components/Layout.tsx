718dd40588c3500a5ccc6bfa1edd70d3556af578
Radu <gith@radu.at>
2022-02-16 18:01:26 +0100

    Fix React Nodes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 import Header from './Header';
 
 type Props = {
-  children: JSX.Element[] | JSX.Element;
+  children: JSX.Element[] | JSX.Element | React.ReactNode;
 };
 
 export default function Layout(props: Props) {
@@ -13,7 +13,7 @@ export default function Layout(props: Props) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div styles={styles} className={styles.container}>
+      <div className={styles.container}>
         <Header />
         <main>{props.children}</main>
       </div>
